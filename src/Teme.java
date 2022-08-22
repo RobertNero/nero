bc16018dbc4b23f1197fd09f65316f5358230f2c
robyr <robyray04@gmail.com>
2022-08-22 20:22:11 +0300

    first commit

---
+++ src/Teme.java
@@ -0,0 +1,16 @@
+public class Teme {
+    public static void main(String[] args) {
+        String[] names = {"Andrei", "Alex", "Mircea", "Oana", "Maria", "", null};
+
+
+    }
+
+    //i.
+    public static void printAllStartingWithA(String[] names){
+        for (String name : names) {
+            if(name != null && name.startsWith("A")) {
+                System.out.println(name);
+            }
+        }
+    }
+}
